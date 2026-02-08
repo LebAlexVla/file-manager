b6a67b2c4d1863af873c66b9e49f19749ba64a16
Alexander Lebedev <lebalexvla@gmail.com>
2026-02-08 17:50:57 +0300

    feat: add console app

--- src/FileManager.Presentation/Program.cs
+++ src/FileManager.Presentation/Program.cs
@@ -1,5 +1,42 @@
+using FileManager.Core.CommandsExecuting;
+using FileManager.Presentation.Parsing;
+
 namespace FileManager.Presentation;
 
-public class Program
+internal class Program
 {
+    private static void Main()
+    {
+        var commander = new Commander();
+        var parserFactory = new ParserFactory();
+        ICommandParserLink parser = parserFactory.Create();
+
+        string? input = " ";
+        do
+        {
+            input = Console.ReadLine();
+            if (input != null)
+            {
+                string[] rawStrings = input.Split(' ');
+                var stream = new StringsStream(rawStrings);
+                CommandParseResult result = parser.Parse(stream);
+
+                if (result is CommandParseResult.Success(var command))
+                {
+                    commander.Run(command);
+                }
+
+                if (result is CommandParseResult.Failure(var error))
+                {
+                    foreach (string rawString in rawStrings)
+                    {
+                        Console.WriteLine(rawString);
+                    }
+
+                    Console.WriteLine(error.Info);
+                }
+            }
+        }
+        while (!string.Equals(input, "q", StringComparison.Ordinal));
+    }
 }
